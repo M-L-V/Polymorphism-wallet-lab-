2e125fab6d6ba8dad167b055ba5e8f422fd19aea
Morgaine <morgainedv@gmail.com>
2020-01-14 14:44:09 +0000

    removes commented out code

--- src/main/java/Wallet.java
+++ src/main/java/Wallet.java
@@ -21,20 +21,5 @@ public class Wallet {
     public void addItem(IScan item){
         this.cards.add(item);
     }
-
-//    public void addItem(CreditCard item) {
-//        this.creditCards.add(item);
-//    }
-//
-//    public void addItem(DebitCard item) {
-//        this.debitCards.add(item);
-//    }
-//
-//    public void addItem(LoyaltyCard item) {
-//        this.loyaltyCards.add(item);
-//    }
-//
-//    public void addItem(Ticket item) {
-//        this.tickets.add(item);
-//    }
+    
 }
